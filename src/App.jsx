70b449ee5d3251a522227fb037a5033ad6c419dc
talitha <backlog.id@outlook.com>
2025-03-16 21:47:26 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import LandingPage from "./pages/LandingPage";
-import SearchRecipe from "./pages/SearchRecipe";
-import ListCategories from "./pages/ListCategories";
-import RecipeDetails from "./pages/RecipeDetails";
-import NoAccessToken from "./components/NoAccessToken";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
       <BrowserRouter>
         <Routes>
           <Route
@@ -19,44 +11,12 @@ function App() {
             element={
               <div>
                 <LandingPage />
-                <NoAccessToken />
-              </div>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/search-recipe"
-            element={
-              <div>
-                <SearchRecipe />
-                <NoAccessToken />
-              </div>
-            }
-          />
-          <Route
-            path="/list-categories"
-            element={
-              <div>
-                <ListCategories />
-                <NoAccessToken />
-              </div>
-            }
-          />
-          <Route
-            path="/recipe-details/:idMeal"
-            element={
-              <div>
-                <RecipeDetails />
-                <NoAccessToken />
               </div>
             }
           />
         </Routes>
-
         <ToastContainer />
       </BrowserRouter>
-    </GoogleOAuthProvider>
   );
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,227 +1,106 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import Navbar from "../components/navigations/Navbar";
-import Footer from "../components/navigations/Footer";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { jwtDecode } from "jwt-decode";
-import { ToastContainer, toast } from "react-toastify";
-
 
 function LandingPage() {
-  const navigate = useNavigate();
-  const [data, setData] = useState([]);
-  const [category, setCategory] = useState([]);
-  const [user, setUser] = useState(null);
+  const [sentence, setSentence] = useState("");
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  async function getUser(userId) {
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // mencegah reload halaman
+    setLoading(true);
     try {
-      const response = await axios.get(`https://dummyjson.com/users/${userId}`);
-      // console.log(response.data);
-      return response.data.firstName;
+      const response = await axios.post("http://194.163.40.96/identification", {
+        text: sentence,
+      });
+      setResult(response.data);
     } catch (error) {
-      console.error("Error fetching user data:", error);
-      return null;
+      console.error("Error:", error);
+      setResult({ error: "Terjadi kesalahan saat identifikasi" });
     }
-  }
-
-  async function fetchData() {
-    try {
-      const response = await axios.get(
-        `https://www.themealdb.com/api/json/v1/1/filter.php?c=seafood`
-      );
-      console.log("meals", response.data.meals);
-      setData(response.data.meals);
-    } catch (error) {
-      console.error("Error fetching meals:", error);
-    }
-  }
-
-  async function fetchCategory() {
-    try {
-      const response = await axios.get(
-        "https://www.themealdb.com/api/json/v1/1/categories.php"
-      );
-      console.log("category", response.data.categories);
-      setCategory(response.data.categories);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-    fetchCategory();
-  }, []);
-
-  async function fetchUser(token) {
-    if (localStorage.getItem("login") === "google") {
-      const decodedToken = jwtDecode(token);
-      console.log(decodedToken);
-      setUser(decodedToken);
-    } else if (localStorage.getItem("login") === "facebook") {
-      try {
-        const response = await axios.get(
-          "https://graph.facebook.com/me?fields=id,name,email,picture",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        console.log("Facebook user data:", response.data);
-        setUser(response.data);
-      } catch (error) {
-        console.error("Error fetching Facebook user data:", error);
-      }
-    } else {
-      try {
-        const response = await axios.get(
-          "https://shy-cloud-3319.fly.dev/api/v1/auth/me",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setUser(response.data.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-  }
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      fetchUser(token);
-    }
-  }, []);
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
+    setLoading(false);
   };
 
   return (
-    <div className="bg-none lg:bg-[url('/bg.jpg')] lg:bg-contain lg:bg-no-repeat overflow-x-hidden">
-      <Navbar transparent={true} />
-      {/* Hero Section */}
-      <div>
-        <div className="w-full h-screen text-main">
-          <div className="absolute top-0 w-full h-screen flex items-center justify-center">
-            <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
-              <div className="container items-center">
-                <div className="text-left w-full md:w-full lg:w-1/2">
-                  <p className=" text-5xl font-bold mb-4">
-                    What would you like to cook today?
-                  </p>
-                  <p className=" text-base font-normal leading-6 mt-6">
-                    Discover the recipe you desire based on the provided
-                    categories or ingredients you have, and create your own
-                    version of the recipe to share it with others! You can do
-                    all of that on NomNom.
-                  </p>
-
-                  <a href={user ? "#recipe" : "/login"}>
-                    <button className="bg-gradient-to-r from-primary to-secondary hover:bg-gradient-to-r hover:from-secondary hover:to-primary rounded-full px-12 py-2 text-base text-white font-semibold mt-4">
-                      Discover Recipes
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
+    <div className="overflow-x-hidden">
+      <div className="container my-20 mx-auto">
+        <h1 className="text-2xl font-bold">User Input</h1>
+        <p className="text-base font-medium mt-4">
+          Masukkan kalimat untuk mengidentifikasi.
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div className="flex mt-4 space-x-4 items-center relative">
+            <textarea
+              id="sentence"
+              name="sentence"
+              rows="4"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500"
+              placeholder="Ketik di sini..."
+              value={sentence}
+              onChange={(e) => setSentence(e.target.value)}
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-[#71B8BA] w-48 px-4 py-3.5 text-white text-base font-medium rounded-lg hover:bg-[#5A9395] cursor-pointer absolute bottom-2 right-8"
+            >
+              Identifikasi
+            </button>
           </div>
-        </div>
-      </div>
-
-      {/* Category Section */}
-      {user ? (
-        <section id="recipe" className="mt-20">
-          <h1 className="container text-3xl text-left font-bold text-main mb-6">
-            Categories
-          </h1>
-          <Slider {...settings}>
-            {category.map((e) => (
-              <div
-                key={e?.idCategory}
-                className="cursor-pointer relative group"
-                onClick={() => {
-                  navigate("/list-categories", {
-                    state: { strCategory: e.strCategory },
-                  });
-                }}
-              >
-                <img
-                  className="w-[280px] object-cover rounded-md"
-                  src={e?.strCategoryThumb}
-                  alt={e?.strCategory}
-                />
-
-                <div className="absolute bottom-2 left-2 text-white font-semibold bg-black bg-opacity-70 px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  {e?.strCategory}
+        </form>
+        {loading && <p className="mt-4">Memproses...</p>}
+        {result && (
+          <div className="mt-4">
+            {result.classification === "Non-Provokasi" ? (
+              <div className="bg-green-50 border-l-4 border-green-400 p-4">
+                <div className="ml-3">
+                  <p className="text-sm text-green-700">
+                    Kalimat yang diinputkan tidak mengandung
+                    <a
+                      href="#"
+                      className="font-bold italic text-green-700 hover:text-green-600"
+                    >
+                      {" "}
+                      Provokasi Doxing
+                    </a>
+                  </p>
                 </div>
               </div>
-            ))}
-          </Slider>
-        </section>
-      ) : (
-        ""
-      )}
-
-      {/* Card Section */}
-      <section id="card" className="container my-20">
-        <h1 className="text-3xl text-left font-bold text-main mb-6">
-          Recipe recommendations
-        </h1>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {Array.isArray(data) && data.length > 0 ? (
-            data.map((e, i) => (
-              <div
-                key={i}
-                className="w-full bg-white shadow rounded-lg cursor-pointer h-full bg-transparent overflow-hidden text-main transition-transform duration-300 hover:scale-[1.05] hover:shadow-lg"
-                onClick={() => {
-                  if (user) {
-                    navigate(`/recipe-details/${e?.idMeal}`, {
-                      state: { idMeal: e?.idMeal },
-                    });
-                  } else {
-                    toast.error(
-                      "Oops.. you need to log in first to view recipe details."
-                    );
-                    navigate("/login");
-                  }
-                }}
-              >
-                <img
-                  className="w-full object-cover h-48"
-                  src={e?.strMealThumb}
-                />
-                <div className="p-3">
-                  <div className="flex flex-col justify-between">
-                    <div className="min-h-10">
-                      <p className="text-base font-semibold leading-tight">
-                        {e?.strMeal}
-                      </p>
-                    </div>
+            ) : (
+              <div className="bg-red-50 border-l-4 border-red-400 p-4">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <svg
+                      className="h-5 w-5 text-red-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm text-red-700">
+                      Kalimat yang diinputkan mengandung
+                      <a
+                        href="#"
+                        className="font-bold italic text-red-700 hover:text-red-600"
+                      >
+                        {" "}
+                        {result.classification}
+                      </a>
+                    </p>
                   </div>
                 </div>
               </div>
-            ))
-          ) : (
-            <p>No recipes available</p>
-          )}
-        </div>
-      </section>
-      <Footer />
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
